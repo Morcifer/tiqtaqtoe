45922010f7699c3f11c038212b905739c4e3bc92
mor.verbin <mor.verbin@coolblue.nl>
2025-05-19 14:21:38 +0200

    It ain't pretty, but it kinda works.

--- src/board.rs
+++ src/board.rs
@@ -1,6 +1,11 @@
-use derive_more::Display;
+use std::collections::{HashMap, HashSet};
 
-#[derive(Clone, Copy, Eq, PartialEq)]
+use derive_more::Display;
+use itertools::Itertools;
+use rand::Rng;
+
+#[derive(Clone, Copy, Debug, Display, Eq, Hash, PartialEq)]
+#[display("{{ row: {}, column: {} }}", row, column)]
 pub struct Position {
     row: usize,
     column: usize,
@@ -12,43 +17,188 @@ impl Position {
     }
 }
 
-#[derive(Clone, Copy, Display, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Display, Eq, PartialEq)]
 #[repr(u8)] // TODO: Does this actually do anything?
 pub enum Token {
     X,
     O,
 }
 
-#[derive(Clone, Copy, Display, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Display, Eq, PartialEq)]
 #[repr(u8)] // TODO: Does this actually do anything?
 pub enum TurnToken {
     X(u8),
     O(u8),
 }
 
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+pub struct SpookyMark(Position, Position, TurnToken);
+
 const BOARD_SIZE: usize = 3;
 
 pub struct Board {
-    board: [[Option<TurnToken>; BOARD_SIZE]; BOARD_SIZE],
+    board: [[Option<TurnToken>; BOARD_SIZE]; BOARD_SIZE], // the board is only updated on collapses
     turn: u8,
+    spooky_marks: Vec<SpookyMark>,
 }
 
 impl Board {
     pub fn new() -> Self {
         Self {
             board: [[None; BOARD_SIZE]; BOARD_SIZE],
-            turn: 0,
+            turn: 1,
+            spooky_marks: vec![],
         }
     }
 
-    pub fn set_token(&mut self, row: usize, column: usize, token: Token) {
-        // TODO: validate un-occupied.
-        match token {
-            Token::X => self.board[row][column] = Some(TurnToken::X(self.turn)),
-            Token::O => self.board[row][column] = Some(TurnToken::O(self.turn)),
+    pub fn set_spooky_mark(&mut self, position_1: Position, position_2: Position, token: Token) {
+        println!(
+            "Putting {token} in {position_1} and {position_2} at turn {}",
+            self.turn
+        );
+
+        let turn_token = match token {
+            Token::X => TurnToken::X(self.turn),
+            Token::O => TurnToken::O(self.turn),
+        };
+
+        if position_1 == position_2 {
+            self.board[position_1.row][position_1.column] = Some(turn_token);
+        } else {
+            self.spooky_marks
+                .push(SpookyMark(position_1, position_2, turn_token));
         }
 
         self.turn += 1;
+    }
+
+    pub fn depth_first_search(
+        &self,
+        position: Position,
+        parent: Option<Position>,
+        visited: &mut HashMap<Position, bool>,
+        parents: &mut HashMap<Position, Option<Position>>,
+    ) -> Option<(Position, Position)> {
+        visited.insert(position, true);
+
+        for SpookyMark(position_1, position_2, _) in &self.spooky_marks {
+            let optional_u;
+
+            if *position_1 == position {
+                optional_u = Some(*position_2);
+            } else if *position_2 == position {
+                optional_u = Some(*position_1);
+            } else {
+                continue;
+            }
+
+            let v = position;
+            let u = optional_u.unwrap();
+
+            if Some(u) == parent {
+                continue;
+            }
+
+            if visited[&u] {
+                // println!("Cycle found, with {u} from {v}");
+                return Some((u, v));
+            }
+
+            parents.insert(u, Some(v));
+
+            if let Some((cycle_start, cycle_end)) =
+                self.depth_first_search(u, parents[&u], visited, parents)
+            {
+                return Some((cycle_start, cycle_end));
+            }
+        }
+
+        None
+    }
+
+    pub fn collapse_loop(&mut self) {
+        // Find if there is a loop - TODO: extract to a separate struct and file
+        let positions = (0..BOARD_SIZE)
+            .flat_map(|row| (0..BOARD_SIZE).map(move |column| Position::new(row, column)))
+            .collect_vec();
+
+        let mut visited: HashMap<Position, bool> = positions.iter().map(|p| (*p, false)).collect();
+        let mut parents: HashMap<Position, Option<Position>> =
+            positions.iter().map(|p| (*p, None)).collect();
+
+        let (mut start_loop, mut end_loop) = (None, None);
+
+        for position in positions {
+            if !visited[&position] {
+                if let Some((start, end)) = self.depth_first_search(
+                    position,
+                    parents[&position],
+                    &mut visited,
+                    &mut parents,
+                ) {
+                    println!("I found a loop from {start} to {end}!");
+                    let mut current = end;
+                    while current != start {
+                        // println!("current is {current}");
+                        current = parents[&current].unwrap();
+                    }
+                    (start_loop, end_loop) = (Some(start), Some(end));
+                    break;
+                }
+            }
+        }
+
+        if start_loop.is_none() {
+            // println!("Is no loop!");
+            return;
+        }
+
+        let (start_loop, end_loop) = (start_loop.unwrap(), end_loop.unwrap());
+
+        // Resolve loop, if there is one, by randomly choosing option.
+        // collapse the first edge
+        let first = *self
+            .spooky_marks
+            .iter()
+            .find(|SpookyMark(p1, p2, _)| {
+                (*p1 == start_loop && *p2 == end_loop) || (*p2 == start_loop && *p1 == end_loop)
+            })
+            .unwrap();
+
+        let mut rng = rand::rng();
+        let choice = rng.random_bool(0.5);
+
+        let position = if choice { first.0 } else { first.1 };
+
+        println!("Collapsing {first:?} on {position}");
+
+        self.board[position.row][position.column] = Some(first.2);
+
+        self.spooky_marks.retain(|m| *m != first);
+
+        let mut collapsed_positions: HashSet<Position> = HashSet::new();
+        collapsed_positions.insert(position);
+
+        while let Some(to_collapse) = self
+            .spooky_marks
+            .iter()
+            .cloned()
+            .find(|m| collapsed_positions.contains(&m.0) || collapsed_positions.contains(&m.1))
+        {
+            self.spooky_marks.retain(|m| *m != to_collapse);
+
+            if self.board[to_collapse.0.row][to_collapse.0.column].is_some() {
+                println!("collapsing {to_collapse:?} on {}", to_collapse.1);
+                self.board[to_collapse.1.row][to_collapse.1.column] = Some(to_collapse.2);
+                collapsed_positions.insert(to_collapse.1);
+            } else if self.board[to_collapse.1.row][to_collapse.1.column].is_some() {
+                println!("collapsing {to_collapse:?} on {}", to_collapse.0);
+                self.board[to_collapse.0.row][to_collapse.0.column] = Some(to_collapse.2);
+                collapsed_positions.insert(to_collapse.0);
+            } else {
+                continue;
+            }
+        }
     }
 
     // TODO: Add tests for a bunch of cases
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,40 @@
 mod board;
 
-use crate::board::{Board, Token};
+use crate::board::{Board, Position, Token};
 
 fn main() {
     println!("Hello, world! Let's play!");
 
+    let game_moves = [
+        (Position::new(0, 0), Position::new(0, 0), Token::X),
+        (Position::new(2, 0), Position::new(1, 1), Token::O),
+        (Position::new(0, 1), Position::new(1, 1), Token::X),
+        (Position::new(2, 1), Position::new(1, 1), Token::O),
+        (Position::new(1, 0), Position::new(2, 0), Token::X),
+        (Position::new(2, 2), Position::new(2, 2), Token::O),
+        (Position::new(0, 1), Position::new(0, 2), Token::X),
+        (Position::new(0, 2), Position::new(1, 1), Token::O),
+    ];
+
     let mut board = Board::new();
 
-    board.set_token(1, 0, Token::X);
-    board.set_token(1, 1, Token::X);
-    board.set_token(1, 2, Token::X);
+    for game_move in game_moves {
+        board.set_spooky_mark(game_move.0, game_move.1, game_move.2);
+        board.collapse_loop();
+        let board_score = board.find_winner();
+        if board_score != (0, 0) {
+            let winner = if board_score.0 == 2 {
+                Token::X
+            } else {
+                Token::O
+            };
 
-    board.set_token(0, 0, Token::O);
-    board.set_token(0, 1, Token::O);
-    board.set_token(0, 2, Token::O);
-
-    println!(
-        "{:?} is the point distribution of {} and {}!",
-        board.find_winner(),
-        Token::X,
-        Token::O
-    );
+            println!(
+                "{:?} is the point distribution of {} and {} - {winner} won!",
+                board_score,
+                Token::X,
+                Token::O,
+            );
+        }
+    }
 }
